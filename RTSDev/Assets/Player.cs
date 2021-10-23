c8bedb34f56785dac2370ca01cc8853704cd8626
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2021-10-23 19:52:06 +0100

    added basic movement to a unit

--- RTSDev/Assets/Player.cs
+++ RTSDev/Assets/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class Player : MonoBehaviour
     public static Player instance;
     public List<Transform> selectedUnits;
     public List<Transform> units;
+
+    public Camera cam;
+    public NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if( Physics.Raycast(ray, out hit) )
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+
+    void moveSelectedUnits()
+    {
+        if(selectedUnits.Count == 1)
+        {
+            //selectedUnits.GetComponent<NavMeshAgent>.SetDestination(hit.point);
+        }
     }
 }
