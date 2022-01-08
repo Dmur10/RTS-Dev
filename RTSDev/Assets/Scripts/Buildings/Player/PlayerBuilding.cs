dc8eb85d67105637628b31e90d8bc00b60f840da
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2022-01-08 21:32:08 +0000

    minor update

---
+++ RTSDev/Assets/Scripts/Buildings/Player/PlayerBuilding.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RTSGame.Buildings.Player
+{
+    public class PlayerBuilding : MonoBehaviour
+    {
+        public BuildingStatTypes.Base baseStats;
+    }
+}
+
