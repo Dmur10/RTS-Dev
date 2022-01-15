ddc026ea94bcf5bb81449b9fbacb64e4de34973f
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2022-01-15 03:16:09 +0000

    buildings UI now functional

--- RTSDev/Assets/Scripts/Interactables/IBuilding.cs
+++ RTSDev/Assets/Scripts/Interactables/IBuilding.cs
@@ -6,13 +6,16 @@ namespace RTSGame.Interactables
 {
     public class IBuilding : Interactable
     {
+        public UI.HUD.PlayerActions actions;
         public override void OnInteractEnter()
         {
+            UI.HUD.ActionFrame.instance.SetActionButtons(actions); 
             base.OnInteractEnter();
         }
 
         public override void OnInteractExit()
         {
+            UI.HUD.ActionFrame.instance.ClearActions();
             base.OnInteractExit();
         }
     }
--- RTSDev/Assets/Scripts/UI/HUD/ActionFrame.cs
+++ RTSDev/Assets/Scripts/UI/HUD/ActionFrame.cs
@@ -1,12 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RTSGame.UI.HUD
 {
     public class ActionFrame : MonoBehaviour
     {
         public static ActionFrame instance = null;
+
+        [SerializeField] private Button actionButton = null;
+        [SerializeField] private Transform layoutGroup = null;
+
+        private List<Button> buttons = new List<Button>();
+
+        private void Awake()
+        {
+                instance = this;
+        
+        }
+
+        public void SetActionButtons(PlayerActions actions)
+        {
+            if (actions.basicUnits.Length>0)
+            {
+                foreach(Units.BasicUnit unit in actions.basicUnits)
+                {
+                    Button btn = Instantiate(actionButton, layoutGroup);
+                    btn.name = unit.name;
+
+                    buttons.Add(btn);
+                }
+            }
+            if (actions.basicBuildings.Length > 0)
+            {
+                foreach (Buildings.BasicBuilding building in actions.basicBuildings)
+                {
+                    Button btn = Instantiate(actionButton, layoutGroup);
+                    btn.name = building.name;
+
+                    buttons.Add(btn);
+                }
+            }
+
+        }
+
+        public void ClearActions()
+        {
+            foreach(Button btn in buttons)
+            { 
+                Destroy(btn.gameObject);
+            }
+            buttons.Clear();
+        }
     }
 
+   
 }
