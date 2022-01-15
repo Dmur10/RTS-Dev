8873b86f8ff7535be349e3a0c004d45e873c1540
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2022-01-15 03:31:55 +0000

    Added builder script
    some clean up

--- RTSDev/Assets/Scripts/Builder.cs
+++ RTSDev/Assets/Scripts/Builder.cs
@@ -6,7 +6,17 @@ namespace RTSGame.Units
 {
     public class Builder : MonoBehaviour
     {
+        public UI.HUD.PlayerActions actions;
 
+        public void OnBuilderSelect()
+        {
+            UI.HUD.ActionFrame.instance.SetActionButtons(actions);
+        }
+
+        public void OnBuilderExit()
+        {
+            UI.HUD.ActionFrame.instance.ClearActions();
+        }
     }
 }
 
