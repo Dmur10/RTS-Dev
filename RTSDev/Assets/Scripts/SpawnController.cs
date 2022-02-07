841b3555cf58eb318d64ffa42fe1b9520a00995f
DarraghMurray <47894701+DarraghMurray@users.noreply.github.com>
2022-02-07 01:14:26 +0000

    more work on spawnController
    Added assets of damaged buildings and tree's from unity asset store

--- RTSDev/Assets/Scripts/SpawnController.cs
+++ RTSDev/Assets/Scripts/SpawnController.cs
@@ -9,8 +9,35 @@ public class SpawnController : MonoBehaviour
     [SerializeField]
     public class Wave
     {
-
+        public string name;
+        public Transform[] Enemies;
+        public int[] count;
+        public float rate;
     }
 
+    private SpawnerState state = SpawnerState.COUNTING;
     public Wave[] waves;
+
+    IEnumerator SpawnWave(Wave wave)
+    {
+        state = SpawnerState.SPAWNING;
+        Debug.Log("Wave Spawned " + wave.name);
+
+        for (int i = 0; i < wave.count.Length; i++)
+        {
+            for(int j = 0; j < wave.count[i]; j++)
+            {
+                SpawnEnemy(wave.Enemies[i]);
+                yield return new WaitForSeconds(1f / wave.rate);
+            }
+        }
+        state = SpawnerState.WAITING;
+        yield break;
+    }
+
+    void SpawnEnemy(Transform enemy)
+    {
+        Debug.Log("Spawn enemy");
+        Instantiate(enemy, transform.position, transform.rotation);
+    }
 }
